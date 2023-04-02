96a36813ff21088d31bbc0c2a60642993848a853
uka yura <ukayura@ukas-MacBook-Air.local>
2023-04-02 12:14:27 +0800

    uka hiisen

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -17,17 +17,27 @@ export default function Product(props) {
     product && (
       <div className="w-full ">
         <div className="container max-w-screen-xl mx-auto border border-black flex box-border px-16 gap-24 py-40">
-          <div className="w-5/12 h-96 border-2 border-gray-500 rounded-[50px] p-12">
-            <img src={product.image} alt="" className="w-full " />
+          <div className="w-5/12 h-96 flex flex-col gap-5 ">
+            <div className="border-2 border-gray-500 rounded-[50px] p-12 w-full h-full">
+              <img src={product.image} alt="" className="w-full " />
+            </div>
+            <div className="w-full flex  gap-5 justify-between">
+              <div className="border-2 border-gray-500 rounded-3xl w-6/12 px-9">
+                <img src={product.image} alt="" className="w-full" />
+              </div>
+              <div className="border-2 border-gray-500 rounded-3xl w-6/12 px-9">
+                <img src={product.image} alt="" className="w-full" />
+              </div>
+            </div>
           </div>
           <div className="w-3/12">
             {" "}
-            <p>{product.name}</p>
-            <p>{product.description}</p>
-            <p>{product.stock}</p>
-            <p>{product.price}</p>
-            <p>{product.sale}</p>
-            <p>{product.category}</p>
+            <p>name: {product.name}</p>
+            <p>descroption: {product.description}</p>
+            <p>stock: {product.stock}</p>
+            <p>price: {product.price}</p>
+            <p>sale: {product.sale}</p>
+            <p>category: {product.category}</p>
             <div>
               {product &&
                 product.spec.map((spec, index) => {
